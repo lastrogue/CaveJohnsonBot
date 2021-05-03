1049e7e2f3e4abd25066d4d435136daf2c7cb5c4
lastrogue <lastrogue>
2021-05-03 00:08:16 -0600

    Initial working prototype

---
+++ CaveJohnson.py
@@ -0,0 +1,25 @@
+# CaveJohnson.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+#client = discord.Client()
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.send("Welcome!")
+
+client.run(TOKEN)
