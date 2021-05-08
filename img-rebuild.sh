958d284048317ded633ab62532542f0a2e1e7287
lastrogue <lastrogue>
2021-05-08 16:05:33 -0600

    added image build script for docker rebuilds

---
+++ img-rebuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Kill all docker containers
+echo "Killing containers....."
+docker kill $(docker ps -q)
+echo "Containers killed."
+# Remove docker containers
+echo "Removing containers....."
+docker rm $(docker ps -a -q)
+echo "Containers removed."
+# Remove docker images
+echo "Removing images....."
+docker rmi $(docker images -q)
+echo "Images removed."
+# Builds based on current docker configuration and files
+echo "Rebuilding Image....."
+docker build -t discord_py_cj .
+echo "Images rebuilt."
+# Starts the docker container in detached mode and always restarts
+echo "Starting Container....."
+docker run -d -restart always discord_py_cj
+echo "Container started."
+echo "Image rebuilt and container started."
