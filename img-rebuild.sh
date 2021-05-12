48041c3e03d7d2c0ba9834c16f6988ea3dbb7822
lastrogue <lastrogue>
2021-05-11 20:05:10 -0600

    updated image rebuild script to account for GLaDOS

--- img-rebuild.sh
+++ img-rebuild.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 # Kill all docker containers
-echo "Killing containers....."
+echo "--------------Killing containers.....--------------"
 docker kill $(docker ps -q)
-echo "Containers killed."
+echo "--------------Containers killed.--------------"
 # Remove docker containers
-echo "Removing containers....."
+echo "--------------Removing containers.....--------------"
 docker rm $(docker ps -a -q)
-echo "Containers removed."
+echo "--------------Containers removed.--------------"
 # Remove docker images
-echo "Removing images....."
+echo "--------------Removing images.....--------------"
 docker rmi $(docker images -q)
-echo "Images removed."
-# Builds based on current docker configuration and files
-echo "Rebuilding Image....."
-docker build -t discord_py_cj .
-echo "Images rebuilt."
-# Starts the docker container in detached mode and always restarts
-echo "Starting Container....."
+echo "--------------Images removed.--------------"
+# Builds cj docker based on current docker configuration and files
+echo "--------------Rebuilding Image.....--------------"
+docker build -t discord_py_cj . # Edit . to point to directory where cj dockerfile is located.
+echo "--------------Images rebuilt.--------------"
+# Builds GLaDOS docker based on current docker configuration and files
+echo "--------------Rebuilding Image.....--------------"
+docker build -t discord_py_glados . # Edit . to point to directory where GLaDOS dockerfile is located.
+echo "--------------Images rebuilt.--------------"
+# Starts the cj docker container in detached mode and always restarts
+echo "--------------Starting cj Container.....--------------"
 docker run -d --restart always discord_py_cj
-echo "Container started."
-echo "Image rebuilt and container started."
+echo "--------------Cj Container started.--------------"
+# Starts the GLaDOS docker container in detached mode and always restarts
+echo "--------------Starting GLaDOS Container.....--------------"
+docker run -d --restart always discord_py_glados
+echo "--------------GLaDOS Container started.--------------"
+echo "--------------Images rebuilt and containers started.--------------"
